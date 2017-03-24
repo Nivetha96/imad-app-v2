aaa7149f4fdf219bf516cf0a18c31766131bf9d6
Nivetha96 <nivethabalsu@gmail.com>
2017-03-24 20:41:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -65,7 +65,7 @@ app.get('/', function (req, res) {
 });
 var pool=new Pool(config);
 app.get('/user-db',function(req,res){
-    pool.query("SELECT * FROM user", function(err,result){
+    pool.query("SELECT * FROM user", function(err,result,rows){
        if(err){res.status(500).send(err.toString());}
        else{
            res.send(JSON.stringify(result));
