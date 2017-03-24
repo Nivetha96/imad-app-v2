d41c652b783ba253d8b3cadeec30dff2bfde82f6
Nivetha96 <nivethabalsu@gmail.com>
2017-03-24 21:37:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,7 +80,7 @@ res.send(counter.toString());
 });
 app.get('/articles/articleName',function(req,res){
     //var articleName=req.params.articleName;
-    pool.query("SELECT * FROM article WHERE title="+req.params.articleName,function(req,res){
+    pool.query("SELECT * FROM article WHERE title='"+req.params.articleName+"'",function(req,res){
        if(err){
            res.status(500).send(err.toString());
        } else{
