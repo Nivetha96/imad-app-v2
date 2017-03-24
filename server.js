21fdb5cbc3e2ec584533109b455b67989199e28c
Nivetha96 <nivethabalsu@gmail.com>
2017-03-24 20:38:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config={
     database:'nivetha96',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:process.env.DB_PASSWORD
+    password:'db-nivetha96-98278'
     };
 var app = express();
 app.use(morgan('combined'));
